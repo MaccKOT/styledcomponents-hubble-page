94b658d028cfca337a1ad1a5e3d5a429c896b973
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-11-25 13:29:51 +0400

    Add images

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <img src='../public/images/logo.svg' alt='' />
         <p>Hello Vite + React!</p>
       </header>
     </div>
